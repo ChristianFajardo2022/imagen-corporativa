577cba3690daeae45d315f1088ab09e5befdd311
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-31 15:05:30 -0500

    counter mobiliario y agencia

--- src/componentes/SeleccionTipoCounter.jsx
+++ src/componentes/SeleccionTipoCounter.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { Selector } from "./Selector";
 
-const cantidadCounter = ["Un counter", "Dos counter", "Tres counter"];
+const cantidadCounter = ["Un counter", "Dos counter", "+ Tres counter"];
 
 export const SeleccionTipoCounter = () => {
-  return (
-    <>
-      <Selector customStyle={`tipoSelect`} title={"counter"} icon={true} />
+  const [open, setOpen] = useState(false);
 
-      {cantidadCounter.map((counter) => (
-        <span>{counter}</span>
-      ))}
-    </>
+  return (
+    <div className="selectOpcions">
+      <Selector
+        handleClick={() => setOpen(true)}
+        customStyle={`tipoSelect`}
+        title={"counter"}
+        icon={true}
+      />
+      <div className="tipoSelect__box flex-col overflow-hidden">
+        {open && (
+          <>
+            {cantidadCounter.map((counter) => (
+              <span className="hover:bg-[--btn-active] hover:text-white w-full effect px-6 py-2">
+                {counter}
+              </span>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
   );
 };

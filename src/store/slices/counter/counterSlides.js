15d4a4f972693d1fdaa1ca24893806d288ca27b9
jeffbaut13 <jeferssonabv@gmail.com>
2024-06-06 12:05:23 -0500

    ajustes tipso de counter

--- src/store/slices/counter/counterSlides.js
+++ src/store/slices/counter/counterSlides.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    Pagina: 3,
+    Pagina: 0,
     resultado: false,
     loading: false,
     formData: {
